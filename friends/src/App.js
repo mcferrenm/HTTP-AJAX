c23cea16e7e95b9142e0d069bab933085a04015d
mcferrenm <mcferrenmax@gmail.com>
2019-01-23 15:59:44 -0500

    Send get request with axios to localserver, log result

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-import "./App.css";
+import axios from "axios";
 
 import AppWrapper from "./styles/AppWrapper";
 
 class App extends Component {
+  state: {
+    friends: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
   render() {
     return <AppWrapper className="app">App</AppWrapper>;
   }
